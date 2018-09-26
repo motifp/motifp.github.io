ed2b371a037a384ee6685d7291ecf435f79736c6
motifp <42493848+motifp@users.noreply.github.com>
2018-09-25 20:23:49 -0400

    delete old js

--- js/calendar.js
+++
@@ -1,9 +0,0 @@
-function create() {
-  var htmlElements = ' ';
-  for (var i = 0; i < 5; i++) {
-    htmlElements += '<div class="box">' + i + '</div>';
-  }
-
-  var container = document.getElementById("box");
-  container.innerHTML = htmlElements;
-}

7b92727993c032f44649dd4be76db08c36424da4
motifp <42493848+motifp@users.noreply.github.com>
2018-10-16 11:14:22 -0400

    change class to id

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $.ajax ({
 $(document).ready(function() {
   $.get('blog-1.txt', function(text) {
     var lines = text.split("\n");
-    $('.blog-1-h-p').html(lines[0]);
+    $('#blog-1-h-p').html(lines[0]);
     $('.blog-1-p1-p').html(lines[1]);
     $('.blog-1-p2-p').html(lines[2]);
   })

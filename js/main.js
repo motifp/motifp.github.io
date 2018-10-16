7f84f6955be8eb08ccc3f9557726d0f3a35b1049
motifp <42493848+motifp@users.noreply.github.com>
2018-10-16 19:48:04 -0400

    add h & p tags

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
   $.get('blog-1.txt', function(text) {
     var lines = text.split("\n");
     $('#blog-1-h-p').html(lines[0]);
+    $('.blog-content').append('<h>' + lines[0] + '</h>');
     $('.blog-1-p1-p').html(lines[1]);
     $('.blog-1-p2-p').html(lines[2]);
   })
@@ -22,7 +23,7 @@ $.ajax ({
   url: 'blog-1.txt',
   dataType: 'text',
   success: function(data) {
-  $('.blog-content').append(data);
+  $('.blog-content').append('<p>' + data + '</p>');
    
   } 
 })

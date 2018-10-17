d4160c483a25c4c1d7c8ae98471349c3dd319ae1
motifp <42493848+motifp@users.noreply.github.com>
2018-10-16 20:16:10 -0400

    make sure other page titles are not changed

--- js/main.js
+++ js/main.js
@@ -13,7 +13,10 @@ $(document).ready(function() {
   $.get('blog-1.txt', function(text) {
     var lines = text.split("\n");
     $('#blog-1-h-p').html(lines[0]);
-    $('.blog-content').prepend('<h>' + lines[0] + '</h>');
+    var pageTitle = $('title');
+    if (pageTitle == "") {
+      $('.blog-content').prepend('<h>' + lines[0] + '</h>');
+        }
     $('title').append(lines[0]);
     $('.blog-1-p1-p').html(lines[1]);
     $('.blog-1-p2-p').html(lines[2]);
